b4b054ded2eb233678323a035d655a76630b40c7
DoughSec <addisondoseck@gmail.com>
2026-02-20 15:01:10 -0500

    base commit

---
+++ src/main/java/com/nomadtrack/nomadtrackserver/NomadTrackServerApplication.java
@@ -0,0 +1,13 @@
+package com.nomadtrack.nomadtrackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadTrackServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NomadTrackServerApplication.class, args);
+    }
+
+}
