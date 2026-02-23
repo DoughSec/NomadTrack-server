dfbc26ab80c7c77a4d9ce779bf0f33675c7fa592
DoughSec <addisondoseck@gmail.com>
2026-02-23 10:01:14 -0500

    changed follower repo

--- src/main/java/com/nomadtrack/nomadtrackserver/repository/FollowerRepository.java
+++ src/main/java/com/nomadtrack/nomadtrackserver/repository/FollowerRepository.java
@@ -1,7 +1,6 @@
 package com.nomadtrack.nomadtrackserver.repository;
 
 import com.nomadtrack.nomadtrackserver.model.Follower;
-import com.nomadtrack.nomadtrackserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,18 +10,18 @@ import java.util.Optional;
 @Repository
 public interface FollowerRepository extends JpaRepository<Follower, Integer> {
     //check if user is already following the user
-    boolean existsByFollowerAndFollowee(User follower, User followee);
+    boolean existsByFollower_IdAndFollowee_Id(Integer followerId, Integer followeeId);
 
     //check follow relationship to unfollow
-    Optional<Follower> findByFollowerAndFollowee(User follower, User followee);
+    Optional<Follower> findByFollower_IdAndFollowee_Id(Integer followerId, Integer followeeId);
 
     //get all users that you follow
-    List<Follower> findAllByFollower(User follower);
+    List<Follower> findAllByFollower_Id(Integer followerId);
 
     //get all followers of a user
-    List<Follower> findAllByFollowee(User followee);
+    List<Follower> findAllByFollowee_Id(Integer followeeId);
 
     //delete follow relationship
-    void deleteByFollowerAndFollowee(User follower, User followee);
+    void deleteByFollower_IdAndFollowee_Id(Integer followerId, Integer followeeId);
 
 }
