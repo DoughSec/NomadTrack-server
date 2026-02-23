df24072784779a8bfa2b7df9a21c4c9b3c148062
DoughSec <addisondoseck@gmail.com>
2026-02-23 15:39:37 -0500

    added wishlist controller and dto class for it

---
+++ src/main/java/com/nomadtrack/nomadtrackserver/controller/WishlistController.java
@@ -0,0 +1,72 @@
+package com.nomadtrack.nomadtrackserver.controller;
+
+import com.nomadtrack.nomadtrackserver.model.Wishlist;
+import com.nomadtrack.nomadtrackserver.model.dto.MapPinDto;
+import com.nomadtrack.nomadtrackserver.model.dto.WishlistCompleteRequest;
+import com.nomadtrack.nomadtrackserver.service.WishlistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nomadTrack/wishlists")
+public class WishlistController {
+    private final WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+    //create Wishlist record
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Wishlist create(@RequestBody Wishlist request) {
+        return wishlistService.create(
+                request.getUser().getId(),
+                request.getTitle(),
+                request.getDescription(),
+                request.getTargetCountry(),
+                request.getTargetCity(),
+                request.getDeadline(),
+                request.isCompleted(),
+                request.getCompletedDate()
+        );
+    }
+
+    //get all Wishlist records
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Wishlist> getAll() {
+        return wishlistService.getAll();
+    }
+
+    //get Wishlist by id
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Wishlist getWishlistById(@PathVariable("id") Integer id) {
+        return wishlistService.getById(id);
+    }
+
+    //mark complete
+    @PatchMapping("/{id}/complete")
+    public Wishlist markComplete(@PathVariable Integer id,
+                                 @RequestBody WishlistCompleteRequest request) {
+        return wishlistService.markComplete(id, request.isCompleted());
+    }
+
+    //update Wishlist record
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Wishlist updateWishlist(@PathVariable("id") Integer id, @RequestBody Wishlist wishlist) {
+        return wishlistService.update(id, wishlist);
+    }
+
+    //delete Wishlist record
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteWishlist(@PathVariable("id") Integer id) {
+        wishlistService.delete(id);
+    }
+
+}
