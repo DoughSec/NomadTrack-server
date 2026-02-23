8b64d6bff80d5b0319d0fd9decec69d5c54e80b1
DoughSec <addisondoseck@gmail.com>
2026-02-23 13:18:01 -0500

    Added base crud user service, might have to change later with auth

--- src/main/java/com/nomadtrack/nomadtrackserver/model/User.java
+++ src/main/java/com/nomadtrack/nomadtrackserver/model/User.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class User {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
