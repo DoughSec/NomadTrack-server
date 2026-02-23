81e1309ceefe3ee712fc48f04be9ee59c1395fc1
DoughSec <addisondoseck@gmail.com>
2026-02-23 11:10:54 -0500

    added wishlist repo

---
+++ src/main/java/com/nomadtrack/nomadtrackserver/repository/WishlistRepo.java
@@ -0,0 +1,10 @@
+package com.nomadtrack.nomadtrackserver.repository;
+
+import com.nomadtrack.nomadtrackserver.model.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WishlistRepo extends JpaRepository<Wishlist, Integer> {
+    List<Wishlist> findAllByUser_Id(Integer userId);
+}
